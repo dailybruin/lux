a97e0c3b82f70244e63ad1e63a646e5cdf6e5a9f
Nathan Smith <nathan@nathansmith.io>
2018-09-11 22:33:13 -0400

    Use jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    '.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupTestFrameworkScriptFile: '<rootDir>/tests/setup.ts',
+};
