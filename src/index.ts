8613295a11feb1f978e8848a9e8f748bf55ece0b
Nathan Smith <nathan@nathansmith.io>
2018-09-18 17:23:29 -0700

    no title casing for now

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ export { default as Footer } from './components/Footer'
 
 // Utils
 export { default as toSentence } from 'utils/toSentence'
-export { default as APTitleCase } from 'utils/APTitleCase'
-export { default as capitalizeWord } from 'utils/capitalizeWord'
