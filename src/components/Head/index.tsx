5a1315aceb9049738370cfe127af880336407f5e
Nathan Smith <nathan@nathansmith.io>
2018-09-23 12:14:24 -0700

    Head stuff

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
 
+/**
+ * Head Properties
+ */
 interface HeadProps {
-  title: string
-  pageTitle?: string
+  /** The name of the website. This is used in setting the page's title. */
+  siteName: string
+  /** The name of the specific page. If provided, the title of the website is `pageName | siteName` */
+  pageName?: string
+  /** A short description of the page, in 150 characters or less. This is used in search results and link previews. */
   description: string
+  /** The canonical URL of the site. */
   url: string
+  /** The image to display for link previews. */
   image: any // idk what require(image) type is
-  twitter: string
 }
 
 export default function Head(props: HeadProps) {
-  const title = props.pageTitle
-    ? `${props.pageTitle} | ${props.title}`
-    : props.title
+  const title = !!props.pageName
+    ? `${props.pageName} | ${props.siteName}`
+    : props.siteName
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={props.description} />
-
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -36,15 +42,15 @@ export default function Head(props: HeadProps) {
         sizes="16x16"
         href={require('./images/favicons/favicon-16x16.png')}
       />
-      {/* <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0080c6" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" /> */}
-
       {/* Canonical URL */}
       <link rel="canonical" href={props.url} />
-
-      {/* Facebook Tags */}
+      {/* Facebook Tags
+      Guide: https://developers.facebook.com/docs/sharing/webmasters
+      Best Practices: https://developers.facebook.com/docs/sharing/best-practices
+      */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={props.url} />
       <meta property="og:title" content={title} />
@@ -52,8 +58,13 @@ export default function Head(props: HeadProps) {
       <meta property="og:description" content={props.description} />
       <meta property="og:site_name" content={title} />
       <meta property="og:locale" content="en_US" />
-
-      {/* Twitter Tags */}
+      {/*
+      Twitter Tags
+      Always prefer `summary_large_image` unless there's a good reason not to.
+      About: https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/abouts-cards
+      About large image summary: https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary-card-with-large-image
+      Markup reference: https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/markup
+      */}
       <meta name="twitter:card" content="summary_large_image" />
       {/* <meta name="twitter:creator" content={props.twitter} /> */}
       <meta name="twitter:site" content="@dailybruin" />
@@ -61,7 +72,6 @@ export default function Head(props: HeadProps) {
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={props.description} />
       <meta name="twitter:image" content={props.image} />
-
       <html lang="en" />
     </Helmet>
   )
