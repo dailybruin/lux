89f5f30b36ecb38d372e99377c0bf304ff21047a
Nathan Smith <nathan@nathansmith.io>
2018-10-09 21:03:27 -0700

    refactor(Article): Fix some syntax

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { css } from 'react-emotion'
-import Image, {ImageProps as Image} from './Image'
+import Image, { ImageProps } from './Image'
 
 enum ContentType {
   Text = 'text',
@@ -16,29 +16,36 @@ interface Text {
  */
 interface ArticleProps {
   /** The paragraphs of content for the story. */
-  content: Array<any>
+  content: any[]
   dropcap: boolean
 }
 
 /** A footer to go at the bottom of every page. */
 export default class Article extends React.Component<ArticleProps> {
-  static defaultProps = {
-    dropcap: false,
-  }
+  public static defaultProps = { dropcap: false }
 
-  render() {
-    const renderedContent = this.props.content.map((content: any, i: number) => {
-      switch (content.type) {
-        case ContentType.Text:
-          const text = content as Text
-          return <p key={i} dangerouslySetInnerHTML={{__html: text.value}} />
-        case ContentType.Image:
-          const image = content as Image
-          return <Image {...image} />
-        default:
-          break
+  public render() {
+    const renderedContent = this.props.content.map(
+      (content: any, i: number) => {
+        switch (content.type) {
+          case ContentType.Text:
+            const text = content as Text
+            return (
+              <p
+                key={i}
+                dangerouslySetInnerHTML={{
+                  __html: text.value,
+                }}
+              />
+            )
+          case ContentType.Image:
+            const image = content as ImageProps
+            return <Image {...image} />
+          default:
+            break
+        }
       }
-    })
+    )
 
     return <article>{renderedContent}</article>
   }
