83ee5482f73116b0374b571a70a20bccec6e2d26
Nathan Smith <nathan@nathansmith.io>
2018-10-12 21:24:16 -0700

    100% width for imgs in Image

--- src/components/Article/index.test.tsx
+++ src/components/Article/index.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render, cleanup } from 'react-testing-library'
+import { cleanup, render } from 'react-testing-library'
 import Article from '.'
 
 const content = [
@@ -9,11 +9,13 @@ const content = [
       '<strong>Lorem</strong> <a href="https://dailybruin.com">ipsum</a> dolor sit amet, consectetur adipiscing elit. Cras porttitor metus in pellentesque bibendum. Fusce rutrum sollicitudin finibus. Proin pellentesque leo enim, sed ultrices diam laoreet ut. Vivamus ut consequat nulla. Aenean sodales enim ut sapien rhoncus, in rhoncus metus mattis. Phasellus sed turpis tristique, tristique augue quis, lobortis est. Proin consectetur volutpat lacus, vitae luctus orci eleifend ac. Aliquam porttitor aliquet purus. Fusce condimentum eu sapien ut malesuada. Sed at nibh purus. Quisque et cursus nisl. Donec sed tincidunt ex.',
   },
   {
-    type: "image",
-    url: "https://dailybruin.com/images/2018/10/web.sp_.mwp_.nbk_.ADX_-640x461.jpg",
-    caption: "Freshman attacker Ashworth Molthen has registered 21 goals on the season, finding the back of the net in 13 of the 16 games so far this year for UCLA men’s water polo. UCLA is one of two teams remaining in the MPSF conference that remains undefeated.",
-    credit: "Amy Dixon/Photo Editor"
-  }
+    caption:
+      'Freshman attacker Ashworth Molthen has registered 21 goals on the season, finding the back of the net in 13 of the 16 games so far this year for UCLA men’s water polo. UCLA is one of two teams remaining in the MPSF conference that remains undefeated.',
+    credit: 'Amy Dixon/Photo Editor',
+    type: 'image',
+    url:
+      'https://dailybruin.com/images/2018/10/web.sp_.mwp_.nbk_.ADX_-640x461.jpg',
+  },
 ]
 
 describe('Article', () => {
