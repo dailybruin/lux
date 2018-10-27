7c693860bdafa08ca932c6c0c4dc201c3d53fe25
Nathan Smith <nathan@nathansmith.io>
2018-10-27 13:56:51 -0700

    Handle positioning and alignment!

---
+++ src/components/CoverPhoto/WhiteLine.tsx
@@ -0,0 +1,10 @@
+import styled from 'react-emotion'
+
+const WhiteLine = styled('div')`
+  height: 0.25rem;
+  width: 3.125rem;
+  margin-right: 0.3125rem;
+  background-color: #fff;
+`
+
+export default WhiteLine
--- src/components/CoverPhoto/index.tsx
+++ src/components/CoverPhoto/index.tsx
@@ -1,10 +1,70 @@
 import * as React from 'react'
-import { css } from 'emotion'
-// import Img from 'gatsby-image';
-// import Header from './Header';
+import { css } from 'react-emotion'
+import Byline from '../Byline'
+import WhiteLine from './WhiteLine'
 
-// const style = css`
-//   z-index: -1;
-// `
+export enum XPosition {
+  Left = 'start',
+  Center = 'center',
+  Right = 'end',
+}
 
-export default class CoverPhoto extends React.Component<any> {}
+export enum YPosition {
+  Top = 'start',
+  Center = 'center',
+  Bottom = 'end',
+}
+
+interface CoverPhotoProps {
+  headline: string
+  imageURL: string
+  authors: string[]
+  xPosition: XPosition
+  yPosition: YPosition
+}
+
+export default class CoverPhoto extends React.Component<CoverPhotoProps> {
+  public render() {
+    const textAlign =
+      this.props.xPosition === XPosition.Center
+        ? 'center'
+        : this.props.xPosition === XPosition.Left
+          ? 'left'
+          : 'right'
+
+    return (
+      <div
+        className={css`
+        width: 100%;
+        height: 100vh;
+        background-image: url("${this.props.imageURL}");
+        display: grid;
+        align-items: ${this.props.yPosition};
+        justify-content: ${this.props.xPosition};
+      `}
+      >
+        <div
+          className={css`
+            margin: 2rem;
+            text-align: ${textAlign};
+            display: flex;
+            flex-direction: column;
+            align-items: ${this.props.xPosition === 'center'
+              ? this.props.xPosition
+              : 'flex-' + this.props.xPosition};
+          `}
+        >
+          <h1
+            className={css`
+              margin: 0;
+            `}
+          >
+            {this.props.headline}
+          </h1>
+          <WhiteLine />
+          <Byline authors={this.props.authors} />
+        </div>
+      </div>
+    )
+  }
+}
