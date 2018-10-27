83243378447acc6013e69e4d214912ab0d52c7c8
Nathan Smith <nathan@nathansmith.io>
2018-10-27 14:44:50 -0700

    feat: Add cover photo (#79)
    * Handle positioning and alignment!
    
    * WhiteLine -> Line
    
    * darkening!
    
    * Background image stuff

--- src/components/CoverPhoto/index.tsx
+++ src/components/CoverPhoto/index.tsx
@@ -1,10 +1,82 @@
 import * as React from 'react'
-import { css } from 'emotion'
-// import Img from 'gatsby-image';
-// import Header from './Header';
+import { css } from 'react-emotion'
+import Byline from '../Byline'
+import Line from './Line'
 
-// const style = css`
-//   z-index: -1;
-// `
+export enum XPosition {
+  Left = 'start',
+  Center = 'center',
+  Right = 'end',
+}
 
-export default class CoverPhoto extends React.Component<any> {}
+export enum YPosition {
+  Top = 'start',
+  Center = 'center',
+  Bottom = 'end',
+}
+
+interface CoverPhotoProps {
+  headline: string
+  imageURL: string
+  authors: string[]
+  xPosition: XPosition
+  yPosition: YPosition
+  darken?: number
+}
+
+export default class CoverPhoto extends React.Component<CoverPhotoProps> {
+  public static defaultProps = {
+    darken: 0,
+  }
+
+  public render() {
+    const textAlign =
+      this.props.xPosition === XPosition.Center
+        ? 'center'
+        : this.props.xPosition === XPosition.Left
+          ? 'left'
+          : 'right'
+
+    return (
+      <div
+        className={css`
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(
+          rgba(0, 0, 0, ${this.props.darken}),
+          rgba(0, 0, 0, ${this.props.darken})
+        ), url("${this.props.imageURL}");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        display: grid;
+        align-items: ${this.props.yPosition};
+        justify-content: ${this.props.xPosition};
+      `}
+      >
+        <div
+          className={css`
+            margin: 2rem;
+            color: #fff;
+            text-align: ${textAlign};
+            display: flex;
+            flex-direction: column;
+            align-items: ${this.props.xPosition === 'center'
+              ? this.props.xPosition
+              : 'flex-' + this.props.xPosition};
+          `}
+        >
+          <h1
+            className={css`
+              margin: 0;
+            `}
+          >
+            {this.props.headline}
+          </h1>
+          <Line />
+          <Byline authors={this.props.authors} />
+        </div>
+      </div>
+    )
+  }
+}
