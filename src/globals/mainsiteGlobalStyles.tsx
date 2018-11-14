ebfc8233a7efa0cb633094735f6d1cdfed265e47
Christopher Aziz <caziz@ucla.edu>
2018-11-13 19:00:26 -0800

    Create Author Card

--- src/globals/mainsiteGlobalStyles.tsx
+++ src/globals/mainsiteGlobalStyles.tsx
@@ -2,17 +2,19 @@ import { css } from 'react-emotion'
 
 /** Web fonts loaded here - only in a browser */
 if (typeof window !== 'undefined') {
-  const WebFontLoader = require('webfontloader');
+  const WebFontLoader = require('webfontloader')
   WebFontLoader.load({
     google: {
-      families: ['Arimo:700']
-    }
-  });
+      families: ['Arimo:700'],
+    },
+  })
 }
 
 /** Reusable common CSS attributes */
-export const headlineFont = 'Arimo';
-export const cardShadow = '0px 2px 4px 0px rgba(0,0,0,0.25)';
+export const headlineFont = 'Arimo'
+export const cardShadow = '0px 2px 4px 0px rgba(0,0,0,0.25)'
+export const gray = 'rgb(197, 197, 197)'
+export const lightGray = 'rgb(242, 242, 242)'
 
 /** CSS breakpoints */
-export const mediaMobileBreakpoint = '@media (max-width: 600px)';
+export const mediaMobileBreakpoint = '@media (max-width: 600px)'
