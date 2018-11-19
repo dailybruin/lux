47e597308c167ef6ccf88fb1c8d457fe171b1955
Nathan Smith <nathan@nathansmith.io>
2018-11-19 12:12:48 -0800

    stuff

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@
 export { default as ArticleGrid } from './components/ArticleGrid'
 export { default as Article } from './components/Article'
 export { default as Byline } from './components/Byline'
-export { default as CoverPhoto } from './components/CoverPhoto'
+export {
+  default as CoverPhoto,
+  XPosition,
+  YPosition,
+} from './components/CoverPhoto'
 export { default as Footer } from './components/Footer'
 export { default as Head } from './components/Head'
 export { default as Headline } from './components/Headline'
@@ -19,3 +23,6 @@ export {
   default as convertEdgesToArticles,
   ArticleCardData,
 } from './utils/convertEdgesToArticles'
+export {
+  default as extractSectionsFromArticleEdges,
+} from './utils/extractSectionsFromArticleEdges'
