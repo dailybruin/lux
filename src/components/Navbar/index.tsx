8955678039851b5f5a7528b697b6044e37da138a
mindip <oacidnim@g.ucla.edu>
2018-11-20 19:01:15 -0800

    box shadows

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ interface NavbarProps {
   /** Logo link */
   logoURL: string
   // /** Pins navbar (or not) */
-  // sticky? : boolean
+  sticky?: boolean
   /** custom style applied to the navbar */
   style?: string
 }
@@ -20,36 +20,42 @@ interface NavbarProps {
 class Navbar extends React.Component<NavbarProps> {
   public static defaultProps = {
     logoURL: '/',
-    // sticky : false,
+    sticky: false,
   }
 
   public render() {
-    // const sticky = css`
-    //                   position: fixed;
-    //                   top: 0;
-    //                 `
+    const sticky = css`
+      position: sticky;
+      top: 0;
+    `
 
     const navbarStyle = css`
-                      display: flex;
-                      justify-content: space-between;
-                      width: 100%;
-                      background-color: white;
-                      ${this.props.style};
-                      /* ${this.props.sticky ? sticky : ''}; */
-                    `
+      box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.15);
+      padding: 10px;
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      background-color: white;
+      ${this.props.style};
+      ${this.props.sticky ? sticky : ''};
+    `
     const linkStyle = css`
-      padding-left: 30px;
+      padding-right: 3vw;
       color: #65696c;
+      font-family: Helvetica, sans-serif;
+      font-weight: 500;
+      font-size: 1.2vw;
       a {
         color: inherit; /* blue colors for links too */
         text-decoration: inherit; /* no underline */
-        font-family: Helvetica, sans-serif;
-        font-weight: 500;
+        font-family: inherit;
+        font-weight: inherit;
+        font-size: inherit;
       }
       a:hover {
         color: #3180c0;
       }
-      align-self: flex-end;
+      align-self: center;
       ${this.props.linkStyle};
     `
     const logoStyle = css`
@@ -57,11 +63,13 @@ class Navbar extends React.Component<NavbarProps> {
         max-width: 15vw;
         overflow: hidden;
         vertical-align: bottom;
+        margin: 0;
       }
     `
     const linksStyle = css`
       display: flex;
       justify-content: space-between;
+      margin-right: 5vw;
     `
 
     const logo = <a href={this.props.logoURL}>{this.props.logo}</a>
@@ -70,8 +78,7 @@ class Navbar extends React.Component<NavbarProps> {
       ? this.props.links.map((link, i) => {
           return (
             <div key={i} className={linkStyle}>
-              {' '}
-              {link}{' '}
+              {link}
             </div>
           )
         })
