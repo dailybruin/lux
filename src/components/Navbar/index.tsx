6947a21af4e1b8d8956e03a3cc5c07014813ce81
mindip <oacidnim@g.ucla.edu>
2018-11-20 17:57:00 -0800

    links and logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,47 +1,85 @@
 import * as React from 'react'
 import { css } from 'react-emotion'
-import { Link } from 'gatsby'
+import Image, { ImageProps } from '../Image'
 
-interface LinkObject {
-  title: string
-  link: string
-}
 interface NavbarProps {
-  /** Array of Link objects */
-  links?: LinkObject[]
+  /** Array of JSX elements */
+  links?: JSX.Element[]
+  /** custom style applied to individual links */
+  linkStyle?: string
+  /** Logo can be text or an image */
+  logo: string | HTMLImageElement
+  /** Logo link */
+  logoURL: string
+  // /** Pins navbar (or not) */
+  // sticky? : boolean
+  /** custom style applied to the navbar */
+  style?: string
 }
 
 class Navbar extends React.Component<NavbarProps> {
+  public static defaultProps = {
+    logoURL: '/',
+    // sticky : false,
+  }
+
   public render() {
+    // const sticky = css`
+    //                   position: fixed;
+    //                   top: 0;
+    //                 `
+
     const navbarStyle = css`
-      display: flex;
-      justify-content: space-between;
-      width: 100%;
+                      display: flex;
+                      justify-content: space-between;
+                      width: 100%;
+                      background-color: white;
+                      ${this.props.style};
+                      /* ${this.props.sticky ? sticky : ''}; */
+                    `
+    const linkStyle = css`
+      padding-left: 30px;
+      color: #65696c;
+      a {
+        color: inherit; /* blue colors for links too */
+        text-decoration: inherit; /* no underline */
+        font-family: Helvetica, sans-serif;
+        font-weight: 500;
+      }
+      a:hover {
+        color: #3180c0;
+      }
+      align-self: flex-end;
+      ${this.props.linkStyle};
+    `
+    const logoStyle = css`
+      img {
+        max-width: 15vw;
+        overflow: hidden;
+        vertical-align: bottom;
+      }
     `
     const linksStyle = css`
       display: flex;
       justify-content: space-between;
     `
+
+    const logo = <a href={this.props.logoURL}>{this.props.logo}</a>
+
     const links = this.props.links
-      ? this.props.links.map(item => {
-          switch (item.link[0]) {
-            case '/':
-              return <a href={item.link}>asdf</a>
-              break
-            case '#':
-              return <a>{item.link}</a>
-              break
-            default:
-              return <div>asdf</div>
-          }
+      ? this.props.links.map((link, i) => {
+          return (
+            <div key={i} className={linkStyle}>
+              {' '}
+              {link}{' '}
+            </div>
+          )
         })
       : undefined
 
     return (
       <div className={navbarStyle}>
-        <div>Logo</div>
-        {/* Causes errors af */}
-        <Link to="/">Test</Link>
+        <div className={logoStyle}>{logo}</div>
         <div className={linksStyle}>{links}</div>
       </div>
     )
