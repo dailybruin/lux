cdbd5061fc94caed809f154e32b6b8a6f43ae423
Richard <richbluehill@gmail.com>
2018-11-26 16:36:26 -0800

    changed name from break to line

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -5,7 +5,7 @@ import Image, { ImageProps } from '../Image'
 enum ContentType {
   Text = 'text',
   Image = 'image',
-  Break = 'break',
+  Line = 'line',
 }
 
 interface Content {
@@ -49,7 +49,7 @@ export default class Article extends React.Component<ArticleProps> {
           case ContentType.Image:
             const image = JSON.parse(content.value) as ImageProps
             return <Image key={i} {...image} />
-          case ContentType.Break:
+          case ContentType.Line:
             return <hr />
           default:
             break
